b14dc1dde33c2901e72fa36d026be6f3fcae8bd5
Huy Mobile <137450364+duyhuy27@users.noreply.github.com>
2024-03-08 16:21:39 +0700

    Lab 1 upload

---
+++ Component/Banner/style.js
@@ -0,0 +1,12 @@
+import { StyleSheet, Text, View } from "react-native";
+import React from "react";
+
+const styles = StyleSheet.create({
+  khung: {
+    width: "100%",
+    height: 200,
+    borderRadius: 10,
+  },
+});
+
+export default styles;
